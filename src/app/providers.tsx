d9020ce0ef579e8192ceeabbaba1e2060f6cce9c
csabi <143322411+csabaracz688@users.noreply.github.com>
2026-01-30 13:21:33 +0100

    Javitas kesz

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,24 +1,8 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
-
-import { routeTree } from '../routeTree.gen'
-
-const queryClient = new QueryClient()
-
-const router = createRouter({
-  routeTree,
-  context: {
-    queryClient,
-  },
-  defaultPreload: 'intent',
-})
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
+import { queryClient } from '../lib/query-client'
+import { router } from '../lib/router'
 
 export default function AppProvider() {
   return (
